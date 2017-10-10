60a971c586a6f86147f78bcc91971cb17a8557cb
Valembois Vincent <vincent_valembois@ens.univ-artois.fr>
2017-10-10 14:59:07 +0200

    feat():continue ps -aux

--- myPsaux.c
+++ myPsaux.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,13 +10,17 @@
 #include <string.h>
 #include <dirent.h>
 
+#include <pwd.h>
+
 #define PROC "/proc/"
 #define CMDLINE "/cmdline"
 #define COMM "/comm"
 #define STAT "/stat"
+#define STATUS "/status"
 
 #define STATE_POSITION 2
 #define READ_CMD_LINE 15
+
 
 char * concat_charactere(char * str , char c){
 
@@ -31,6 +36,16 @@ char * concat_charactere(char * str , char c){
     return newChaine;
 }
 
+FILE * myFopen(char * file){
+    FILE * fp = fopen(file,"r");
+
+    if(fp == NULL){
+        perror("Le fichier n'existe pas \n");
+        exit(1);
+    }
+
+    return fp;
+}
 
 int openFile(char * chemin){
     int idfichier=open(chemin,O_RDONLY);
@@ -51,6 +66,14 @@ char * recupPath(char * processus){
     return path;
 }
 
+char * copy_path(char * path){
+    char * path_cmdline = malloc(sizeof(char)*strlen(path)+1);
+
+    strcpy(path_cmdline,path);
+
+    return path_cmdline;
+}
+
 char * recup_comm(char * path){
     char * comm = malloc(sizeof(char));
     char c;
@@ -69,16 +92,13 @@ char * recup_comm(char * path){
     return comm;
 }
 
-
 void afficher_cmdLine(char * path){
-    printf("--Cmdline : ");
+ printf("--Cmdline : ");
     int nb_char = 0;
     char * cmdline = NULL;
     char c;
-    char * path_cmdline = malloc(sizeof(char)*strlen(path)+1);
-
-    strcpy(path_cmdline,path);
-
+    
+    char * path_cmdline = copy_path(path);
 
     int idfichier = openFile(strcat(path_cmdline,CMDLINE));
     while (read(idfichier,&c,sizeof(char)) != 0){ 
@@ -95,22 +115,18 @@ void afficher_cmdLine(char * path){
         cmdline = recup_comm(path);
     }
   
-    printf("%s \n",cmdline);
+    printf("%-15.30s \n",cmdline);
     close(idfichier);
 }
-
-
-
 
 void afficher_state(char * path){
     printf("--State : ");
 
-    char * chemin = strcat(path,STAT);
     char c;
 
     int nb_mot = 1;
 
-    int idfichier = openFile(chemin);
+    int idfichier = openFile(strcat(path,STAT));
     while (read(idfichier,&c,sizeof(char)) != 0 && nb_mot <= STATE_POSITION){ 
         nb_mot = c ==' ' ? nb_mot+1 : nb_mot;
     }
@@ -119,31 +135,70 @@ void afficher_state(char * path){
     close(idfichier);
 }
 
+int recup_uid(char * path){
+
+    FILE * fp = myFopen(strcat(path,STATUS));
+
+    int nb_ligne = 0;
+    int parcour_ligne = 0;
+    char * string_to_read = malloc(sizeof(char)*256);
+    char * uid = NULL;
+    int nb_mot = 0;
+
+
+    while(fgets(string_to_read, 256, fp) && nb_ligne < 9){
+        nb_ligne++;
+    }
+
+    while(parcour_ligne < strlen(string_to_read)){
+        if(nb_mot >= 2) break;
+        if(string_to_read[parcour_ligne] == '\t'){nb_mot++;}
+        else if(nb_mot == 1){
+            uid = concat_charactere(uid,string_to_read[parcour_ligne]);
+        }
+         parcour_ligne++;
+    }
+
+    return atoi(uid);
+}
+
+void afficher_user(char * path){
+    printf("--User : ");
+ 
+    struct passwd * pwd;
+
+    pwd = getpwuid(recup_uid(path));
+
+    if ( pwd != NULL)
+        printf(" %-8.8s \n", pwd->pw_name);
+}
+
 void detailsProcessus(char * processusPID){
-	
-	printf("-----------------------------------\n");
-	char * processus_path = recupPath(processusPID);
-    afficher_cmdLine(recupPath(processusPID));
-    afficher_state(processus_path);
+    
+    printf("-----------------------------------\n");
+    char * processus_path = recupPath(processusPID);
 
+    afficher_user(copy_path(processus_path));
+    afficher_cmdLine(copy_path(processus_path));
+    afficher_state(copy_path(processus_path));
 
-	printf("-----------------------------------\n");
+    printf("-----------------------------------\n");
 }
 
 void readProc(){
-	struct dirent *lecture;
+    struct dirent *lecture;
     DIR *rep;
     char *ptr;
     rep = opendir(PROC);
     while ((lecture = readdir(rep))) {
-    	if(lecture->d_type == 4 && strtol(lecture->d_name, &ptr, 10)!=0){
-        	printf("Numero processus : %s\n", lecture->d_name);
-        	detailsProcessus(lecture->d_name);
+        if(lecture->d_type == 4 && strtol(lecture->d_name, &ptr, 10)!=0){
+            printf("Numero processus : %s\n", lecture->d_name);
+            detailsProcessus(lecture->d_name);
         }
     }
     closedir(rep);
 }
 
 int main(void){
-	readProc();
+    readProc();
 }
