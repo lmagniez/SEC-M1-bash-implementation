3c6ea4464541cece5f5af124c5464676a1ca0863
Loïck Magniez <loick_magniez@ens.univ-artois.fr>
2017-10-15 17:32:32 +0200

    feat(): ls, can read the type of file and the rights for a given path

--- myls.c
+++ myls.c
@@ -5,39 +5,179 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <pwd.h>
 
-
-#define syserror(x) perror(errormsg[x])
+#define syserror(x) perror(errormsg[x]), exit(x)
 #define ROUGE(m) "\033[01;31m"m"\033[0m"
 #define VERT(m) "\033[01;32m"m"\033[0m"
 
 #define MAX_PATH 10
+#define RIGHT_LEN 10
+
 
 char *errormsg[]={
 	"No error",//0
 	ROUGE("Invalid options"),//1
 	ROUGE("Problem with opendir"),//2
+	ROUGE("Problem with stat"),//3
 	
 	
 };
 
+/*
+ * Makes a path ending with a /
+ * /!\ returns malloc, don't forget to free
+ */
+char *add_slash_to_path(char *path){
+	
+	char *res;
+	
+	if(path[strlen(path)-1]!='/'){
+		int size_path_tmp=strlen(path)+2;
+		
+		res = malloc(sizeof(char)*size_path_tmp);
+		strcpy(res, path);
+		res[size_path_tmp-2]='/';
+		res[size_path_tmp-1]='\0';
+	}
+	else{
+		res = malloc(sizeof(char)*(strlen(path)+1));
+		strcpy(res, path);
+	}
+	return res;
+}
+
+/*
+ * If a path starts with ~, the function replace it with the user's path.
+ * /!\ returns malloc, don't forget to free
+ */
+char *replace_tilde(char *path){
+	char *res;
+	
+	struct passwd *pw = getpwuid(getuid());
+	const char *homedir = pw->pw_dir;
+	printf("homedir %s \n", homedir);
+	
+	if(path[0]=='~'){
+		int size_path=strlen(path)+strlen(homedir)-1;
+		res = malloc(sizeof(char)*size_path);
+		strcpy(res,homedir);//delete the ~
+		strcat(res,path+1);
+		printf("PATH FULL: %s \n", res);
+	}
+	return res;
+}
 
 
+/*
+ * ls -l to a path
+ */
 void list_file(char *path){
+	
+	char * path_tmp;
+	char * path_tmp_full;
 	
 	DIR* directory;
 	struct dirent* readen_file;
 	printf("ls -l %s \n",path);
 	
-	directory = opendir(path);
+	
+	path_tmp = add_slash_to_path(path);
+	path_tmp_full = replace_tilde(path_tmp);
+	
+	directory = opendir(path_tmp_full);
 	if(!directory)syserror(2);
 	
 	while(readen_file=readdir(directory)){
 		
+		//to display
 		char *filename;//
 		char *fullpath;//
-		char* path_tmp;
+		//char * path_tmp;
 		int file_type;
+		int uid;//
+		char *username;
+		int gid;//
+		char *groupname;
+		char rights[]="----------";
+		
+		int nb_hard_link;		
+		int file_size;
+		
+		char* modif_month;
+		int modif_day;
+		char* modif_time;
+		
+		//in process
+		mode_t st_mode;
+		struct stat *buf_stat;
+		
+		int fullpath_size;
+		
+		
+		//Get the full path
+		filename = readen_file->d_name;
+		fullpath_size = strlen(path_tmp_full) + strlen(filename);
+		
+		fullpath = malloc(sizeof(char)*(fullpath_size+1));
+		strcpy(fullpath,path_tmp_full);
+		strcat(fullpath,filename);
+		
+		
+		printf("fullpath -> %s\n", fullpath);
+		
+		buf_stat = malloc(sizeof(struct stat));
+		
+		if(stat(fullpath, buf_stat)==-1){
+			syserror(3);
+		}
+		
+		st_mode = buf_stat->st_mode;
+		
+		
+		if(S_IRUSR&st_mode);
+			rights[1]='r';
+		if(S_IWUSR&st_mode)
+			rights[2]='w';
+		if(S_IXUSR&st_mode)
+			rights[3]='x';
+		if(S_IRGRP&st_mode)
+			rights[4]='r';
+		if(S_IWGRP&st_mode)
+			rights[5]='w';
+		if(S_IXGRP&st_mode)
+			rights[6]='x';
+		if(S_IROTH&st_mode)
+			rights[7]='r';
+		if(S_IWOTH&st_mode)
+			rights[8]='w';
+		if(S_IXOTH&st_mode)
+			rights[9]='x';
+		
+		//int file_type;
+		if(S_ISREG(st_mode)){
+			rights[0]='f';
+		}
+		else if(S_ISDIR(st_mode)){
+			rights[0]='d';
+		}
+		else if(S_ISBLK(st_mode)){
+			rights[0]='b';
+		}
+		else if(S_ISLNK(st_mode)){
+			rights[0]='l';
+		}
+		else if(S_ISFIFO(st_mode)){
+			rights[0]='p';
+		}
+		else if(S_ISSOCK(st_mode)){
+			rights[0]='s';
+		}
+		else if(S_ISCHR(st_mode)){
+			rights[0]='c';
+		}
+		
+		/*
 		int uid;//
 		char *username;
 		int gid;//
@@ -49,54 +189,26 @@ void list_file(char *path){
 		char* modif_month;
 		int modif_day;
 		char* modif_time;
+		*/
 		
-		struct stat *buf_stat;
-		
-		int fullpath_size;
-		
-		
-		//add a slash to the path if necessary
-		if(path[strlen(path)-1]!='/'){
-			path_tmp = malloc(sizeof(char)*(strlen(path)+1));
-			strcpy(path_tmp, path);
-			path_tmp[strlen(path_tmp)-1]='/'; 
-		}
-		
-		//Get the full path
-		filename = readen_file->d_name;
-		fullpath_size = strlen(path_tmp) + strlen(filename);
-		
-		fullpath = malloc(sizeof(char)*fullpath_size);
-		strcat(fullpath,path_tmp);
-		strcat(fullpath,filename);
+		printf("readen file: %s \n", readen_file->d_name); 
+		printf("rights: %s\n",rights);
 		
 		
 		
-		buf_stat = malloc(sizeof(struct stat));
-		
-		
-		printf("full path: %s\n",fullpath);
-		
-		
-		//if(stat(filename, bufstat))
-		
-		
-		
-		
-		
-		
-		printf("readen file: %s %c\n", readen_file->d_name, readen_file->d_type); 
-		
-		free(path_tmp);
 		free(fullpath);
 		free(buf_stat);
 		
 		//stat -t compil.sh 
 		//NAME TAILLE BLOCS _ UID GID _ _ NBLIEN _ _ _ _ _ _ _ BLOCE/S
-	
+		
+		
+		
 	}
 	
 	closedir(directory);
+	free(path_tmp);
+	free(path_tmp_full);
 	
 }
 
@@ -128,50 +240,13 @@ struct stat {
       >>>>>>>>>struct timespec st_ctim;  // time of last status change 
 */
 
-/*
-
-		   S_ISUID     04000   set-user-ID bit
-           S_ISGID     02000   set-group-ID bit (see below)
-           S_ISVTX     01000   sticky bit (see below)
-
-           S_IRWXU     00700   owner has read, write, and execute permission
-           S_IRUSR     00400   owner has read permission
-           S_IWUSR     00200   owner has write permission
-           S_IXUSR     00100   owner has execute permission
-
-           S_IRWXG     00070   group has read, write, and execute permission
-           S_IRGRP     00040   group has read permission
-           S_IWGRP     00020   group has write permission
-           S_IXGRP     00010   group has execute permission
-
-           S_IRWXO     00007   others (not in group) have read,  write,  and
-                               execute permission
-           S_IROTH     00004   others have read permission
-           S_IWOTH     00002   others have write permission
-           S_IXOTH     00001   others have execute permissio
-
-*/
 
 
-/*
-	Different type of ls:
-		   b      block (buffered) special
-           c      character (unbuffered) special
-           d      directory
-           p      named pipe (FIFO)
-           f      regular file
-           l      symbolic link; this is never true if the  -L  option
-                  or  the -follow option is in effect, unless the sym‐
-                  bolic link is broken.  If you  want  to  search  for
-                  symbolic links when -L is in effect, use -xtype.
-           s      socket
-           D      door (Solaris)
-*/
 
 int main(int argc, char *argv[]){
 
 	//path variables
-	char** path = malloc(sizeof(char*)*MAX_PATH); 
+	char** tab_path = malloc(sizeof(char*)*MAX_PATH); 
 	int nb_path = 0; 
 	int cur_size_path = MAX_PATH;
 	
@@ -206,12 +281,12 @@ int main(int argc, char *argv[]){
 		else{
 			if(nb_path>=cur_size_path){
 				cur_size_path=cur_size_path*2;
-				char** path_tmp = malloc(sizeof(char*)*cur_size_path);
-				memcpy(path_tmp,path,sizeof(path)*cur_size_path/2);
-				free(path);
-				path=path_tmp;
+				char** tab_path_tmp = malloc(sizeof(char*)*cur_size_path);
+				memcpy(tab_path_tmp,tab_path,sizeof(tab_path)*cur_size_path/2);
+				free(tab_path);
+				tab_path=tab_path_tmp;
 			}
-			path[nb_path++]=argv[i];
+			tab_path[nb_path++]=argv[i];
 		}
 		
 	}
@@ -225,7 +300,7 @@ int main(int argc, char *argv[]){
 		printf(">> %s\n", path[i]);
 	}*/
 	
-	
-	list_file("../bash-sec");
-	
+	//list_file("/home/loick/Bureau/Nouveau dossier");
+	list_file("~/Bureau");
+	free(tab_path);
 }
