fcf5c0af482925cb843931d654808173484d7d79
vincent valembois <vincent_valembois@ens.univ-artois.fr>
2017-12-28 23:53:38 +0100

    feat():finish philo

--- philo_dir/philo.c
+++ philo_dir/philo.c
@@ -30,7 +30,7 @@ sem_t mutex; // exclusion mutuelle
 sem_t * philosophe; //semaphore par philosophe
 value_etat * etat; //etat par philosophe
 pthread_t * philosopheThreadTab; //thread par philosophe
-
+int * mangertab;
 
 // --------------------------------
 // ------------ TOOL --------------
@@ -48,11 +48,13 @@ void init(int numPhilosophe){
   nombrePhilosophe = numPhilosophe;
   philosophe = malloc(sizeof(sem_t)*numPhilosophe);
   etat = malloc(sizeof(value_etat)*numPhilosophe); 
+  mangertab = malloc(sizeof(int)*numPhilosophe); 
   philosopheThreadTab = malloc(sizeof(pthread_t)*numPhilosophe); 
 
   for (int i=0;i<numPhilosophe;i++){
     sem_init(&(philosophe[i]), 0, 1);
     etat[i] = PENSE;
+    mangertab[i]=0;
   }
 
   sem_init(&mutex, 0, 1);
@@ -76,16 +78,46 @@ void destroy(){
 // ------------- V/P --------------
 // --------------------------------
 void P(sem_t * mut){
-  sem_wait(mut);
+  int erreur = sem_wait(mut);
+  if(erreur == -1) {
+    couleurRouge("sem_wait()");
+    exit(0);
+  }
 }
 
 void V(sem_t * mut){
-  sem_post(mut);
+  int erreur = sem_post(mut);
+  if(erreur == -1) {
+    couleurRouge("sem_post()");
+    exit(0);
+  }
 }
 
 // --------------------------------
 // ----------- ACTION -------------
 // --------------------------------
+void couleur(char * message,int num){
+  switch(num%5){
+    case 0:
+      couleurVert(message);
+    break;
+    case 1:
+      couleurJaune(message);
+    break;
+    case 2:
+      couleurBleuFonce(message);
+    break;
+    case 3:
+      couleurGris(message);
+    break;
+    case 4:
+      couleurClair(message);
+    break;
+  }
+}
+
+
+
 void penser(int currentPhilo){
   char * message = malloc(256);
   char * num = intToChar(currentPhilo);
@@ -96,7 +128,7 @@ void penser(int currentPhilo){
 
   strcat(message,pense);
 
-  couleurJaune(message);
+  couleur(message,currentPhilo);
 
   free(message);
   free(num);
@@ -112,7 +144,7 @@ void manger(int currentPhilo){
 
   strcat(message,mange);
 
-  couleurBleuFonce(message);
+  couleur(message,currentPhilo);
 
   free(message);
   free(num);
@@ -128,7 +160,7 @@ void afaim(int currentPhilo){
 
   strcat(message,faim);
 
-  couleurClair(message);
+  couleur(message,currentPhilo);
 
   free(message);
   free(num);
@@ -144,11 +176,13 @@ void arriver(int currentPhilo){
 
   strcat(message,arrive);
 
-  couleurVert(message);
+  couleur(message,currentPhilo);
 
   free(message);
   free(num);
 }
+
+
 
 void quitter(int currentPhilo){
   char * message = malloc(256);
@@ -160,7 +194,7 @@ void quitter(int currentPhilo){
 
   strcat(message,quitte);
 
-  couleurRouge(message);
+  couleur(message,currentPhilo);
 
   free(message);
   free(num);
@@ -181,11 +215,33 @@ int droite(int num){
 // --------------------------------
 // ---------- BAGUETTES -----------
 // --------------------------------
+
+
+
 void testBaguettes(int num){
   if((etat[num] == AFAIM ) && (etat[gauche(num)] != MANGE ) && (etat[droite(num)] != MANGE )){
       etat[num] = MANGE;
       manger(num);
+      mangertab[num] = mangertab[num] + 1; 
       V(&(philosophe[num]));
+   } 
+}
+
+void testBaguetteVoisinGauche(int num){
+  if((etat[num] == AFAIM ) && (etat[gauche(num)] != MANGE )){
+      V(&(philosophe[num]));
+      etat[num] = MANGE;
+      manger(num);
+      mangertab[num] = mangertab[num] + 1; 
+   } 
+}
+
+void testBaguetteVoisinDroite(int num){
+  if((etat[num] == AFAIM ) && (etat[droite(num)] != MANGE )){
+      V(&(philosophe[num]));
+      etat[num] = MANGE;
+      manger(num);
+      mangertab[num] = mangertab[num] + 1; 
    } 
 }
 
@@ -205,10 +261,11 @@ void poser_baguettes(int num){
    P(&mutex); 
 
    etat[num] = PENSE;
+
    penser(num);
    
-   testBaguettes(gauche(num)); 
-   testBaguettes(droite(num));
+   testBaguetteVoisinGauche(gauche(num)); 
+   testBaguetteVoisinDroite(droite(num));
    V(&mutex);
 }
 
@@ -218,11 +275,12 @@ void poser_baguettes(int num){
 void * action_philosophe(void * arg){
   int number = *(int*)arg;
 
-  etat[number] = PENSE;
   arriver(number);
-  
+
   for(int i = 0 ; i < nb_tour ; i++){
+    sleep(1);
     prendre_baguettes(number);
+    sleep(1);
     poser_baguettes(number);
   }
 
